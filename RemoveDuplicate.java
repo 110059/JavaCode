acd8a010928cb12fd5faa127262ffacdfa9fad97
Ved <vedmaurya@gmail.com>
2019-08-25 23:15:30 +0800

    java basic code

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -1,4 +1,47 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class RemoveDuplicate {
-
+   public static void main(String[] args) {
+	   
+	   List<Integer> nums = new ArrayList<Integer>();
+	   nums.add(10);
+	   nums.add(12);
+	   nums.add(6);
+	   nums.add(8);
+	   nums.add(12);
+	   nums.add(6);
+	   
+	   System.out.println(nums);
+	   
+	   /* method 1
+	   List<Integer> numsUnique = nums.stream().distinct().collect(Collectors.toList());
+	   System.out.println(numsUnique);
+	   */
+	   
+	   
+	   
+	   /* method 2
+	   List<Integer> numsUnique = new ArrayList<Integer>();
+	   for(Integer num : nums) {
+		   if(numsUnique.indexOf(num) == -1) {
+			   numsUnique.add(num);
+		   }
+	   }	   
+	   System.out.println(numsUnique);
+	   */
+	   
+	   
+	   
+	   /* method 3 */	   	    
+	   Set<Integer> numsUnique = new HashSet<Integer>();
+	   for(Integer num : nums) {
+		   numsUnique.add(num); 
+	   }	
+	   System.out.println(numsUnique);
+	   
+   }
 }
